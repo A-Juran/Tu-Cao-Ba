1ea34bf24bc12c751426aa9b2c710fd81b8a5b7b
JuRan <2369668922@qq.com>
2025-02-12 11:10:20 +0800

    feat: 页面-调整-自适应

--- Tu-Cao-Ba/src/view/Container.jsx
+++ Tu-Cao-Ba/src/view/Container.jsx
@@ -3,8 +3,39 @@ import "../assets/css/container.scss";
 import { Button, Input } from "@nextui-org/react";
 import { HeartIcon, Smile, SendMsg } from "../assets/icons/icons";
 import ChatList from "./container/chatItem";
+// websocket
+import { io } from "socket.io-client";
+
+function initWebSocketConnect() {
+  const socket = io("http://localhost:8888/");
+
+  //connect
+  socket.on("connect", (data) => {
+    console.log("connect success");
+    socket.emit("chat", {
+      type: "MS:TEXT",
+      userId: "785919644501544960",
+      fromUserId: "785919644501544960", // jiangjing
+      toUserId: "786600935907659776", // taohua
+      content: "你好，我是江景！",
+    });
+  });
+
+  //connect_error
+  socket.on("connect_error", (error) => {
+    console.error("WebSocket 连接失败:", error);
+  });
+
+  //dis_connect
+  socket.on("disconnect", (reason) => {
+    console.log("WebSocket 连接断开:", reason);
+  });
+}
 
 export default function Container() {
+  //连接webSocket
+  initWebSocketConnect();
+
   return (
     <>
       <div className="container-box">
--- Tu-Cao-Ba/src/view/Header.jsx
+++ Tu-Cao-Ba/src/view/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
             variant="faded"
             color="success"
           >
-            Ok
+            success
           </Chip>
         </div>
       </div>
